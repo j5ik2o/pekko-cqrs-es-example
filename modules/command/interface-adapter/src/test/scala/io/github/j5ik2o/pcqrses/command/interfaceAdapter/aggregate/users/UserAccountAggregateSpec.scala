679af2d3cd84e1a3a77351d0d5028350da996f3b
Junichi Kato <j5ik2o@gmail.com>
2025-09-14 00:17:32 +0900

    test: UserAccountAggregateの包括的なテストスイートを実装
    - UserAccountAggregateSpecを実装し、全状態と操作をカバー
    - UserAccountTestHelperにテストヘルパーメソッドを追加
    - 未作成/作成済み/削除済みの各状態でのテストを実装
    - Create/Get/Rename/Delete全コマンドの動作確認テストを追加
    - カバレッジ向上のためエラーケースのテストも含む

--- modules/command/interface-adapter/src/test/scala/io/github/j5ik2o/pcqrses/command/interfaceAdapter/aggregate/users/UserAccountAggregateSpec.scala
+++ modules/command/interface-adapter/src/test/scala/io/github/j5ik2o/pcqrses/command/interfaceAdapter/aggregate/users/UserAccountAggregateSpec.scala
@@ -1,7 +1,109 @@
 package io.github.j5ik2o.pcqrses.command.interfaceAdapter.aggregate.users
 
-import org.scalatest.funsuite.AnyFunSuiteLike
+import com.typesafe.config.{Config, ConfigFactory}
+import io.github.j5ik2o.pcqrses.command.domain.users.UserAccountId
+import io.github.j5ik2o.pcqrses.command.interfaceAdapter.contract.users.UserAccountProtocol.Command
+import io.github.j5ik2o.pcqrses.command.interfaceAdapter.test.ActorSpec
+import org.apache.pekko.actor.testkit.typed.scaladsl.TestProbe
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.concurrent.Eventually
+import org.scalatest.matchers.should.Matchers
 
-class UserAccountAggregateSpec extends AnyFunSuiteLike {
+import java.nio.file.Files
+import java.util.{Comparator, UUID}
 
+object UserAccountAggregateSpec {
+  val id: String = UUID.randomUUID().toString
+
+  val config: Config = ConfigFactory.parseString(
+    s"""
+       |pekko {
+       |  persistence {
+       |    journal {
+       |      plugin = "pekko.persistence.journal.inmem"
+       |      inmem {
+       |        class = "org.apache.pekko.persistence.journal.inmem.InmemJournal"
+       |        plugin-dispatcher = "pekko.actor.default-dispatcher"
+       |      }
+       |    }
+       |    snapshot-store {
+       |      plugin = "pekko.persistence.snapshot-store.local"
+       |      local {
+       |        dir = "target/snapshot/$id"
+       |      }
+       |    }
+       |  }
+       |  test {
+       |    single-expect-default = 5s
+       |  }
+       |}
+       |""".stripMargin
+  ).withFallback(ConfigFactory.load())
+
+}
+
+class UserAccountAggregateSpec extends ActorSpec(UserAccountAggregateSpec.config)
+  with UserAccountTestHelper
+  with Matchers with Eventually
+  with BeforeAndAfterAll {
+  override def afterAll(): Unit = {
+    super.afterAll()
+    val snapshotDir = new java.io.File(s"target/snapshot/${UserAccountAggregateSpec.id}")
+    if (snapshotDir.exists()) {
+      Files
+        .walk(snapshotDir.toPath)
+        .sorted(Comparator.reverseOrder())
+        .forEach(Files.delete(_))
+    }
+  }
+
+  /**
+   * 直接spawnしたアクターにコマンドを送信
+   */
+  override def sendCommand[Reply](
+                                   userAccountId: UserAccountId,
+                                   createCommand: UserAccountId => Command,
+                                   probe: TestProbe[Reply]
+                                 ): Unit = {
+    val aggregate = spawn(UserAccountAggregate(userAccountId))
+    aggregate ! createCommand(userAccountId)
+  }
+
+  "UserAccountAggregate" - {
+    "ユーザアカウントが未作成の状態" - {
+      "Createコマンドを受信したとき" - {
+        "新しいユーザアカウントを作成できる" in
+          testCreateUserAccountOnNotCreated()
+      }
+      
+      "Getコマンドを受信したとき" - {
+        "NotFoundを返す" in
+          testGetUserAccountOnNotCreated()
+      }
+    }
+    
+    "ユーザアカウントが作成済みの状態" - {
+      "Getコマンドを受信したとき" - {
+        "ユーザアカウント情報を返す" in
+          testGetUserAccountOnCreated()
+      }
+      
+      "Renameコマンドを受信したとき" - {
+        "ユーザアカウントの名前を変更できる" in
+          testRenameUserAccountOnCreated()
+      }
+      
+      "Deleteコマンドを受信したとき" - {
+        "ユーザアカウントを削除できる" in
+          testDeleteUserAccountOnCreated()
+      }
+    }
+    
+    "ユーザアカウントが削除済みの状態" - {
+      "Getコマンドを受信したとき" - {
+        "NotFoundを返す" in
+          testGetUserAccountOnDeleted()
+      }
+    }
+  }
 }
--- modules/command/interface-adapter/src/test/scala/io/github/j5ik2o/pcqrses/command/interfaceAdapter/aggregate/users/UserAccountTestHelper.scala
+++ modules/command/interface-adapter/src/test/scala/io/github/j5ik2o/pcqrses/command/interfaceAdapter/aggregate/users/UserAccountTestHelper.scala
@@ -1,12 +1,12 @@
 package io.github.j5ik2o.pcqrses.command.interfaceAdapter.aggregate.users
 
 import io.github.j5ik2o.pcqrses.command.domain.users.{EmailAddress, FirstName, LastName, UserAccountId, UserAccountName}
-import io.github.j5ik2o.pcqrses.command.interfaceAdapter.contract.users.UserAccountProtocol.{Command, Create, CreateReply, CreateSucceeded}
+import io.github.j5ik2o.pcqrses.command.interfaceAdapter.contract.users.UserAccountProtocol._
 import io.github.j5ik2o.pcqrses.command.interfaceAdapter.test.ActorSpec
 import org.apache.pekko.actor.testkit.typed.scaladsl.TestProbe
 import org.scalatest.matchers.should.Matchers
 
-trait UserAccountTestBase { this: ActorSpec & Matchers =>
+trait UserAccountTestHelper { this: ActorSpec & Matchers =>
   def sendCommand[Reply](
                           userAccountId: UserAccountId,
                           createCommand: UserAccountId => Command,
@@ -14,7 +14,7 @@ trait UserAccountTestBase { this: ActorSpec & Matchers =>
                         ): Unit
 
   /**
-   * テストヘルパー: スタッフを作成する
+   * テストヘルパー: ユーザアカウントを作成する
    */
   protected def createUserAccount(
                              userAccountId: UserAccountId = UserAccountId.generate(),
@@ -28,6 +28,166 @@ trait UserAccountTestBase { this: ActorSpec & Matchers =>
       probe
     )
     probe.expectMessageType[CreateSucceeded]
+  }
+
+  /**
+   * テストヘルパー: ユーザアカウントが未作成の状態でCreateコマンドを受信したときのテスト
+   */
+  protected def testCreateUserAccountOnNotCreated(): Unit = {
+    val userAccountId = UserAccountId.generate()
+    val name = UserAccountName(FirstName("太郎"), LastName("山田"))
+    val email = EmailAddress("taro@example.com")
+    
+    val probe = createTestProbe[CreateReply]()
+    sendCommand(
+      userAccountId,
+      id => Create(id, name, email, probe.ref),
+      probe
+    )
+    
+    val reply = probe.expectMessageType[CreateSucceeded]
+    reply.id shouldBe userAccountId
+  }
+
+  /**
+   * テストヘルパー: 作成済みユーザアカウントにGetコマンドを送信したときのテスト
+   */
+  protected def testGetUserAccountOnCreated(): Unit = {
+    val userAccountId = UserAccountId.generate()
+    val name = UserAccountName(FirstName("花子"), LastName("鈴木"))
+    val email = EmailAddress("hanako@example.com")
+    
+    // まずユーザアカウントを作成
+    createUserAccount(userAccountId, name, email)
+    
+    // Getコマンドを送信
+    val getProbe = createTestProbe[GetReply]()
+    sendCommand(
+      userAccountId,
+      id => Get(id, getProbe.ref),
+      getProbe
+    )
+    
+    val reply = getProbe.expectMessageType[GetSucceeded]
+    reply.value.id shouldBe userAccountId
+    reply.value.name shouldBe name
+    reply.value.emailAddress shouldBe email
+  }
+
+  /**
+   * テストヘルパー: 作成済みユーザアカウントの名前を変更するテスト
+   */
+  protected def testRenameUserAccountOnCreated(): Unit = {
+    val userAccountId = UserAccountId.generate()
+    val originalName = UserAccountName(FirstName("太郎"), LastName("山田"))
+    val email = EmailAddress("taro@example.com")
+    val newName = UserAccountName(FirstName("次郎"), LastName("山田"))
+    
+    // まずユーザアカウントを作成
+    createUserAccount(userAccountId, originalName, email)
+    
+    // Renameコマンドを送信
+    val renameProbe = createTestProbe[RenameReply]()
+    sendCommand(
+      userAccountId,
+      id => Rename(id, newName, renameProbe.ref),
+      renameProbe
+    )
+    
+    val reply = renameProbe.expectMessageType[RenameSucceeded]
+    reply.id shouldBe userAccountId
+    
+    // 名前が変更されたことを確認
+    val getProbe = createTestProbe[GetReply]()
+    sendCommand(
+      userAccountId,
+      id => Get(id, getProbe.ref),
+      getProbe
+    )
+    
+    val getReply = getProbe.expectMessageType[GetSucceeded]
+    getReply.value.name shouldBe newName
+  }
+
+  /**
+   * テストヘルパー: 作成済みユーザアカウントを削除するテスト
+   */
+  protected def testDeleteUserAccountOnCreated(): Unit = {
+    val userAccountId = UserAccountId.generate()
+    val name = UserAccountName(FirstName("太郎"), LastName("山田"))
+    val email = EmailAddress("taro@example.com")
+    
+    // まずユーザアカウントを作成
+    createUserAccount(userAccountId, name, email)
+    
+    // Deleteコマンドを送信
+    val deleteProbe = createTestProbe[DeleteReply]()
+    sendCommand(
+      userAccountId,
+      id => Delete(id, deleteProbe.ref),
+      deleteProbe
+    )
+    
+    val reply = deleteProbe.expectMessageType[DeleteSucceeded]
+    reply.id shouldBe userAccountId
+    
+    // 削除後にGetコマンドを送信すると、NotFoundになることを確認
+    val getProbe = createTestProbe[GetReply]()
+    sendCommand(
+      userAccountId,
+      id => Get(id, getProbe.ref),
+      getProbe
+    )
+    
+    getProbe.expectMessageType[GetNotFoundFailed]
+  }
+
+  /**
+   * テストヘルパー: 未作成のユーザアカウントにGetコマンドを送信したときのテスト
+   */
+  protected def testGetUserAccountOnNotCreated(): Unit = {
+    val userAccountId = UserAccountId.generate()
+    
+    val probe = createTestProbe[GetReply]()
+    sendCommand(
+      userAccountId,
+      id => Get(id, probe.ref),
+      probe
+    )
+    
+    val reply = probe.expectMessageType[GetNotFoundFailed]
+    reply.id shouldBe userAccountId
+  }
+
+  /**
+   * テストヘルパー: 削除済みユーザアカウントにGetコマンドを送信したときのテスト
+   */
+  protected def testGetUserAccountOnDeleted(): Unit = {
+    val userAccountId = UserAccountId.generate()
+    val name = UserAccountName(FirstName("太郎"), LastName("山田"))
+    val email = EmailAddress("taro@example.com")
+    
+    // ユーザアカウントを作成して削除
+    createUserAccount(userAccountId, name, email)
+    
+    val deleteProbe = createTestProbe[DeleteReply]()
+    sendCommand(
+      userAccountId,
+      id => Delete(id, deleteProbe.ref),
+      deleteProbe
+    )
+    deleteProbe.expectMessageType[DeleteSucceeded]
+    
+    // 削除済みユーザアカウントにGetコマンドを送信
+    val getProbe = createTestProbe[GetReply]()
+    sendCommand(
+      userAccountId,
+      id => Get(id, getProbe.ref),
+      getProbe
+    )
+    
+    val reply = getProbe.expectMessageType[GetNotFoundFailed]
+    reply.id shouldBe userAccountId
   }
 
 }
